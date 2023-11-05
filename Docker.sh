1911c9b3c649aa5621dabbeef482277486901a86
Aryan Singh <74867817+aryansingh920@users.noreply.github.com>
2023-11-05 13:43:09 +0530

    2023-11-05 13:43:09 - docker.sh updated

--- Docker.sh
+++ Docker.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# Prompt the user for the volume path
-read -p "Enter the volume path (e.g., /path/to/your/volume): " volume_path
+while true; do
+    # Prompt the user for the volume path
+    read -p "Enter the volume path (e.g., /path/to/your/volume): " volume_path
 
-if [ -z "$volume_path" ]; then
-    echo "Volume path cannot be empty. Exiting."
-    exit 1
-fi
+    if [ -z "$volume_path" ]; then
+        echo "Volume path cannot be empty. Please provide a valid path."
+    elif [ ! -d "$volume_path" ]; then
+        echo "The specified path does not exist or is not a directory. Please provide a valid path."
+    else
+        break  # Valid path provided, exit the loop
+    fi
+done
 
 # Build the Docker image
 docker build -t excel-manipulator .
 
 # Run the Docker container with the volume mount option
-docker run -p 3005:3005 -d -v "$volume_path":/app/output excel-manipulator
+docker run -p 3000:3000 -d -v "$volume_path":/app/output excel-manipulator
